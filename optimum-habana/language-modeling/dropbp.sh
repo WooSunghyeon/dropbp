a529732ecb0757b8613f54153f7590b46bc5f8d8
root <root@pggaudi-s018-nicl.nfra.io>
2024-10-22 18:11:21 +0000

    update optimum-habana example codes

--- optimum-habana/language-modeling/dropbp.sh
+++ optimum-habana/language-modeling/dropbp.sh
@@ -1,6 +1,6 @@
 for train_bs in {11..1}
 do
-        HABANA_VISIBLE_DEVICES=1 python3 ../gaudi_spawn.py --world_size 1 --master_port 29501 run_lora_clm.py \
+        HABANA_VISIBLE_DEVICES=1 python3 run_lora_clm.py\
                 --deepspeed ds_config.json \
                 --model_name_or_path meta-llama/Meta-Llama-3-8B \
                 --dataset_name timdettmers/openassistant-guanaco \
@@ -23,5 +23,5 @@ do
                 --throughput_warmup_steps 3\
                 --drop_rate 0.5\
                 --measure_time_memory \
-                --throughput_path /home/wshey/throughput/dropbp-0.5.txt \
+                --throughput_path /home/wshey/throughput/dropbp-0.5.txt
 done
